0a0d9cba76fa40021a709cee2d3cdb9022d3220d
osamahabib5 <osama.habib@khi.iba.edu.pk>
2021-03-10 17:10:12 +0500

    Working on Appointment for Demo Schedule

--- src/components/pages/BookaDemo/ScheduleDemo.js
+++ src/components/pages/BookaDemo/ScheduleDemo.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import SelectedTeacher from './SelectedTeacher'
 import axios from "axios";
@@ -7,16 +7,35 @@ import baseUrl from '../../../baseUrl/baseUrl'
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 function ScheduleDemo() {
-    const getTimeUrl = baseUrl + "/api/demo/getTimes/58?date=21-03-15"
+    const [date, setdate] = useState("");
+    const getTimeUrl = baseUrl + "/api/demo/getTimes/58"
     const getDateUrl = baseUrl + "/api/demo/getDays/58"
-    const DaysList = [0,1,2,3,4,5,6];
+    const [selectedday, setSelectedday] = useState(null)
+    const DaysList = [0, 1, 2, 3, 4, 5, 6];
     const { timeslots, getTimeSlots, getTeacherDays, days } = useContext(TutorsContext);
     const fetchTimeSlots = async () => {
-        await axios.get(getTimeUrl).then(response => {
+        await axios.get(getTimeUrl, {
+            params: {
+                date: date
+            }
+        }).then(response => {
             getTimeSlots(response.data.data.times)
         }).catch(error => {
             console.log("Error: " + error)
         })
+    }
+    const handleDayClick = (day, { selected }) => {
+        setSelectedday(selected ? undefined : day)
+        var d = new Date(selected ? undefined : day);
+        var month = '' + (d.getMonth() + 1);
+        var day = '' + d.getDate();
+        var year = d.getFullYear().toString().substring(2, 4);
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+        setdate([year, month, day].join('-'));
+        fetchTimeSlots();
     }
     const fetchDays = async () => {
         await axios.get(getDateUrl).then(response => {
@@ -31,38 +50,37 @@ function ScheduleDemo() {
         for (i = 0; i < days.length; i++) {
             if (days[i] == "Sunday") {
                 index = DaysList.indexOf(0);
-                DaysList.splice(index,1)
+                DaysList.splice(index, 1)
             }
             else if (days[i] == "Monday") {
                 index = DaysList.indexOf(1);
-                DaysList.splice(index,1)
+                DaysList.splice(index, 1)
             }
             else if (days[i] == "Tuesday") {
                 index = DaysList.indexOf(2);
-                DaysList.splice(index,1)
+                DaysList.splice(index, 1)
             }
             else if (days[i] == "Wednesday") {
                 index = DaysList.indexOf(3);
-                DaysList.splice(index,1)
+                DaysList.splice(index, 1)
             }
             else if (days[i] == "Thursday") {
                 index = DaysList.indexOf(4);
-                DaysList.splice(index,1)
+                DaysList.splice(index, 1)
             }
             else if (days[i] == "Friday") {
                 index = DaysList.indexOf(5);
-                DaysList.splice(index,1)
+                DaysList.splice(index, 1)
             } else if (days[i] == "Saturday") {
                 index = DaysList.indexOf(6);
-                DaysList.splice(index,1)
+                DaysList.splice(index, 1)
             }
         }
         return DaysList;
     };
     useEffect(() => {
-        fetchTimeSlots();
         fetchDays();
-    }, [])
+    }, [date])
     return (
         <Container>
             <Row>
@@ -76,21 +94,19 @@ function ScheduleDemo() {
                 </Col>
                 <Col>
                     <DayPicker
-                        initialMonth={new Date(2020, 3)}
+                        initialMonth={new Date(2021, 3)}
                         disabledDays={[{ daysOfWeek: getDayNumber() }]}
-                        selectedDays={[
-                            new Date(2020, 3, 13),
-                            new Date(2020, 3, 6),
-                        ]}
+                        selectedDays={selectedday}
+                        onDayClick={handleDayClick}
                     />
                 </Col>
                 <Col>
                     <div className="timeslots">
-                        {timeslots.length > 0 ? timeslots.map(data => {
+                        {timeslots && selectedday ? timeslots.map((data, index) => {
                             return (
-                                <Button style={{ background: "white", color: "black", height: "48px", width: "164px", border: "black" }}>{data}</Button>
+                                <Button key={index} style={{ background: "white", color: "black", height: "48px", width: "164px", border: "black" }}>{data}</Button>
                             )
-                        }) : ""}
+                        }) : "" }
                     </div>
                 </Col>
             </Row>
